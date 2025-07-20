742dcdf555679fd7d4d88b904a1100e6a3da84cc
Soumili Saha <logitechsoumili@gmail.com>
2025-07-20 14:53:53 +0530

    Solved problem 5 in Python

---
+++ 5_Rotate_Array_By_One/solution.py
@@ -0,0 +1,9 @@
+def shift(arr):
+        for i in range(1):
+                temp = arr[-1]
+                for j in range(len(arr) - 1, -1, -1):
+                        arr[j] = arr[j - 1]
+                arr[0] = temp
+        print(arr)
+arr = list(map(int, input("Enter array elements: ").split()))
+shift(arr)
