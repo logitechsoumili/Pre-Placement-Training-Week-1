ca2ce7c5016a05d004aa9d3eef83782790ec5337
Soumili Saha <logitechsoumili@gmail.com>
2025-07-20 12:24:07 +0530

    Solved problem 8 in Python and C

---
+++ 8_Check_String_Palindrome/solution.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char s[100];
+	printf("Enter string: ");
+	gets(s);
+	int len = strlen(s);
+	char rev[len + 1];
+
+	
+	for (int i = 0; i < len; i++){
+		rev[i] = s[len - 1 - i];
+	}
+	rev[len] = '\0';
+
+	if (strcmp(rev, s) == 0)
+    		printf("true\n");
+	else
+    		printf("false\n");
+
+	
+	return 0;
+}
---
+++ 8_Check_String_Palindrome/solution.py
@@ -0,0 +1,6 @@
+s = input("Enter a string: ")
+rev = s[::-1]
+if rev == s:
+	print("true")
+else:
+	print("false")
